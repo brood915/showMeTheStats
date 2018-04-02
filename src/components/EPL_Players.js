a589b518c9d0e40482b890f14521d03afd596c89
Kevin Jang <brood915@gmail.com>
2018-04-02 15:51:13 -0400

    epl_players now shows diff contents based on props

--- src/components/EPL_Players.js
+++ src/components/EPL_Players.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
 const EPL_Players = (props) => {
+    
+    let content = '';
+
+    if (props.loading) {
+        content = <div>Loading...</div>
+    }
+    else if (props.errors) {
+        content = <div>Something unexpected happened! Please try again later!</div>
+    }
+
+    else {
+        content = props.players.map(each => <li key={each.id}>{each['web_name']}</li>);
+    }
             return (
 			<div className = "epl-players">
-				{props.players.map(each => <li key={each.id}>{each['web_name']}</li>)}				
+				{content}				
 			</div>		
         )
 }
