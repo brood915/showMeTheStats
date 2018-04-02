768a4e3bdb0edb6689670a82cf9758c398431680
Kevin Jang <brood915@gmail.com>
2018-04-02 17:31:08 -0400

    Added more data to players table

--- src/components/EPL_Players.js
+++ src/components/EPL_Players.js
@@ -19,16 +19,35 @@ const EPL_Players = (props) => {
                         <th>Shirts #</th>
                         <th>Team</th>
                         <th>Pos</th>
-                        
+                        <th>Goals</th>
+                        <th>Assists</th>
+                        <th>Own Goals</th>
+                        <th>Goals/hr</th>
+                        <th>Assists/hr</th>
+                        <th>G+A/hr</th>
+                        <th>Minutes Played</th>
                     </tr>
                         {props.players.map((each,index) => {
-                            const playerTeam = props.teams.filter(team => each.team_code == team.code);
-                            const position = props.positions.filter(position => each.element_type == position.id);
+                            const hrs = each.minutes/60;
+                            const goals_per_hr = (((each.goals_scored/hrs) * 100)/100).toFixed(2);
+                            const assists_per_hr = (each.assists/hrs).toFixed(2);
+                            const goals_and_assists_per_hr = ((each.assists + each.goals_scored)/hrs).toFixed(2);
+                            const playerTeam = props.teams.filter(team => each.team_code === team.code);
+                            const position = props.positions.filter(position => each.element_type === position.id);
+
                             return (<tr key={each.id.toString()}>
-                                <td>{each.web_name}</td>
-                                <td>{each.squad_number}</td>
-                                <td>{playerTeam[0].short_name}</td>
-                                <td>{position[0].singular_name_short}</td>
+                                <td className = "players_table_box">{each.web_name}</td>
+                                <td className = "players_table_box">{each.squad_number}</td>
+                                <td className = "players_table_box">{playerTeam[0].short_name}</td>
+                                <td className = "players_table_box">{position[0].singular_name_short}</td>
+                                <td className = "players_table_box">{each.goals_scored}</td>
+                                <td className = "players_table_box">{each.assists}</td>
+                                <td className = "players_table_box">{each.own_goals}</td>
+                                <td className = "players_table_box">{goals_per_hr === "NaN" ? "0.00" : goals_per_hr}</td>
+                                <td className = "players_table_box">{assists_per_hr === "NaN" ? "0.00" : assists_per_hr}</td>
+                                <td className = "players_table_box">{goals_and_assists_per_hr === "NaN" ? "0.00" : goals_and_assists_per_hr}</td>
+                                <td className = "players_table_box">{each.minutes}</td>
+                                <td className = "players_table_box"></td>
                             </tr>);
                         })}
                 </tbody>
